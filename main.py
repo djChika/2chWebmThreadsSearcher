470a89285a1f7ede942691d31e49d740e01b0d3c
djChika <etka12865%>
2018-06-30 15:43:26 +0300

    added 'Танц' filter to include keywords removed beautiful soup from code

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import re
 import urllib.request
 import webbrowser
 
-from bs4 import BeautifulSoup
 
 # SETTINGS
 MAIN_URL = 'https://2ch.hk/b/threads.json'
@@ -14,7 +13,8 @@ INCLUDE_KEYWORDS = r'([Ww][Ee][Bb][Mm])|' \
                    r'([Цц][Уу][Ии][Ьь])|' \
                    r'([ВвШш][Ее][Бб][Мм])|' \
                    r'([Mm][Pp][4])|' \
-                   r'([Ьь][Зз][4])'
+                   r'([Ьь][Зз][4])|' \
+                   r'([Тт][Аа][Нн][Цц])'
 
 EXCLUDE_KEYWORDS = r'([Фф][Аа][Пп])|' \
                    r'([Ff][Aa][Pp])|' \
@@ -43,9 +43,8 @@ threads = board['threads']
 urls_list = []
 
 for thread in threads:
-    op_post = thread['comment']
-    soup = BeautifulSoup(op_post, "html.parser").get_text()
-    op_post_length = len(soup)
+    op_post = thread['subject']
+    op_post_length = len(op_post)
     thread_posts_count = thread['posts_count']
     if re.findall(INCLUDE_KEYWORDS, op_post) \
             and not re.findall(EXCLUDE_KEYWORDS, op_post) \
